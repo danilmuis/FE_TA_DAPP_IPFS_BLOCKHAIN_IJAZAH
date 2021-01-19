7bf99861b1c01bf536ee5ee7f1ca3f0db5930bdb
danilmuis <daniltelkom@gmail.com>
2021-01-19 15:32:51 +0700

    remove vueaxios

--- src/main.js
+++ src/main.js
@@ -50,10 +50,10 @@ const router = new VueRouter({
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-axios.defaults.withCredentials = true;
-Vue.use(VueAxios, axios);
+// import axios from 'axios';
+// import VueAxios from 'vue-axios';
+// axios.defaults.withCredentials = true;
+// Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.prototype.$Chartist = Chartist;
